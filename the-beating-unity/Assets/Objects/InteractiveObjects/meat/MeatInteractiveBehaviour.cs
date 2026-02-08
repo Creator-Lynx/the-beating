b97b464c96d7800531416ffa4545cc65f0dcc42d
CreatorLynx <1_linx_1@mail.ru>
2026-02-08 04:44:12 +0300

    sleep state in meat prefab and add func to turnOn

--- the-beating-unity/Assets/Objects/InteractiveObjects/meat/MeatInteractiveBehaviour.cs
+++ the-beating-unity/Assets/Objects/InteractiveObjects/meat/MeatInteractiveBehaviour.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class MeatInteractiveBehaviour : MonoBehaviour, IInteractivable
 {
     [SerializeField] AudioSource audioSource;
+    [SerializeField] Collider _collider;
+    [SerializeField] Rigidbody _rigidbody;
 
     void OnCollisionEnter(Collision collision)
     {
         audioSource.Play();
+    }
+
+    public void EnableDropState()
+    {
+        _collider.enabled = true;
+        _rigidbody.useGravity = true;
     }
 }
