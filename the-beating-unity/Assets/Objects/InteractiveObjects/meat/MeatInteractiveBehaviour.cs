011d719d32d98a2686b4fdf35cbcd58dab73aa0b
CreatorLynx <1_linx_1@mail.ru>
2026-02-08 18:36:51 +0300

    meat sound pitch randomize

--- the-beating-unity/Assets/Objects/InteractiveObjects/meat/MeatInteractiveBehaviour.cs
+++ the-beating-unity/Assets/Objects/InteractiveObjects/meat/MeatInteractiveBehaviour.cs
@@ -5,9 +5,11 @@ public class MeatInteractiveBehaviour : MonoBehaviour, IInteractivable
     [SerializeField] AudioSource audioSource;
     [SerializeField] Collider _collider;
     [SerializeField] Rigidbody _rigidbody;
+    [SerializeField] float pitchShiftExtremum = 0.1f;
 
     void OnCollisionEnter(Collision collision)
     {
+        audioSource.pitch = 1 + Random.Range(-1f, 1f) * pitchShiftExtremum;
         audioSource.Play();
     }
 
