ad62556b74048d7211c3018c23ac44515274d78c
CreatorLynx <1_linx_1@mail.ru>
2026-02-08 20:34:38 +0300

    Raycast interaction
    - wrote Raycast system that check interaction objects
    	if find that, show its hint in console
    	if press 'interact' when Interactable is found call its
    	Interact()
    - added system to Player prefab
    - tested raycast
    - added new input

---
+++ the-beating-unity/Assets/GameSystems/PlayerSystem/PlayerInteract.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float raycastDistance = 3f;
+    [SerializeField] LayerMask layerMask;
+
+    
+    void FixedUpdate()
+    {
+        RaycastHit raycastHit;
+        if(Physics.Raycast(transform.position, transform.forward, out raycastHit, raycastDistance, layerMask))
+        {
+            //call hint if new
+            //write current interactivable
+            if(interactivable == null)
+            {
+                interactivable = raycastHit.collider.GetComponent<IInteractivable>();
+                //call show hint
+                Debug.Log(interactivable.GetInteractionHint());
+            }
+        }
+        else 
+        {
+            interactivable = null;
+            //call hide of hint
+        }
+        
+    }
+
+    IInteractivable interactivable = null;
+    void Update()
+    {
+        if(interactInputAction.WasPressedThisFrame()) 
+        //call interact of current interactivable if it is
+        if(interactivable != null) interactivable.Interact();
+    }
+
+    InputAction interactInputAction;
+    void Awake()
+    {
+        interactInputAction = InputSystem.actions.FindAction("Interact");
+    }
+}
