f190826d7f57fbbfe117dadb20c0cb938ec230c5
CreatorLynx <1_linx_1@mail.ru>
2026-02-08 05:03:46 +0300

    MEAT drop TREE behaviour
    - new type of tree -- meat tree. added prefab variant
    - when meat tree is cuted down it drop its meat. modified its behaviour
    - meat on the tree before cutting cannot be used and static

--- the-beating-unity/Assets/Objects/InteractiveObjects/tree/TreeBehaviour.cs
+++ the-beating-unity/Assets/Objects/InteractiveObjects/tree/TreeBehaviour.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class TreeBehaviour : MonoBehaviour
 {
-    bool haveMeat = false;
+    [SerializeField] bool haveMeat = false;
     [SerializeField] GameObject wholeTree;
     [SerializeField] GameObject bottomTree;
     [SerializeField] GameObject topTree;
+    [SerializeField] MeatInteractiveBehaviour[] meats;
 
     public void Death()
     {
         wholeTree.SetActive(false);
         bottomTree.SetActive(true);
         topTree.SetActive(true);
-        if (!haveMeat)
+        if (haveMeat)
         {
-            
+            foreach (MeatInteractiveBehaviour meat in meats)
+            {
+                meat.EnableDropState();
+            }
         }
     }
 }
