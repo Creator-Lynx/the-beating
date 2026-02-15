6d29feac23bf8b9db0ce14d6dcef09d1b9f4b003
CreatorLynx <1_linx_1@mail.ru>
2026-02-15 10:19:21 +0300

    transform pos for meat in hand&fields in script

--- the-beating-unity/Assets/Objects/Weapons/hand/HandResourceInventory.cs
+++ the-beating-unity/Assets/Objects/Weapons/hand/HandResourceInventory.cs
@@ -1,19 +1,33 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class HandResourceInventory : MonoBehaviour
 {
+    //global
+    public static HandResourceInventory resourceInventory;
+
+    [Header("Meat inventory")]
     [SerializeField] int meatCount = 0;
     [SerializeField] int maxMeatCount = 3;
 
+    [Space(20)]
+    [Header("Hand Animatons")]
     [SerializeField] Animator animator;
     [SerializeField] string handInteractionTriggerName = "Grab";
     [SerializeField] string handWalkBoolName = "IsWalk";
     [SerializeField] string handResetTriggerName = "SetEmpty";
     int interactTriggerId, resetTriggerId, walkBoolId;
-    public static HandResourceInventory resourceInventory;
-
+    //input operate for animation "walk"
     InputAction movingAction;
+
+    [Space(20)]
+    [Header("Meat positions")]
+    [SerializeField] Transform[] meatPositions = new Transform[3];
+    [Header("Meat transition timings")]
+    [SerializeField] float transitionDelay = 0.05f;
+    [SerializeField] float transitionDuration = 0.4f;
+
 
     void Start()
     {
@@ -35,6 +49,19 @@ public class HandResourceInventory : MonoBehaviour
         }
         return false;
     }
+
+    public bool TryToIncrementMeatCount(Transform transform)
+    {
+        if(meatCount < maxMeatCount)
+        {
+            meatCount++;
+            animator.SetTrigger(interactTriggerId);
+            return true;
+        }
+        return false;
+    }
+
+
 
     /// <summary>
     /// Clear player resource inventory. 
