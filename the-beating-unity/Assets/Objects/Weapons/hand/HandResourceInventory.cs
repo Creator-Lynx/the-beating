ae331526c52d6addfb0baaeab1bb837d37b6acf4
CreatorLynx <1_linx_1@mail.ru>
2026-02-15 07:13:15 +0300

    hand grab animation and inventory cleaning
    - also added walk animation to hand
    - added func to clear player meat inventory and also something can get
      current carried meat count by this method

--- the-beating-unity/Assets/Objects/Weapons/hand/HandResourceInventory.cs
+++ the-beating-unity/Assets/Objects/Weapons/hand/HandResourceInventory.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HandResourceInventory : MonoBehaviour
 {
     [SerializeField] int meatCount = 0;
     [SerializeField] int maxMeatCount = 3;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] Animator animator;
+    [SerializeField] string handInteractionTriggerName = "Grab";
+    [SerializeField] string handWalkBoolName = "IsWalk";
+    [SerializeField] string handResetTriggerName = "SetEmpty";
+    int interactTriggerId, resetTriggerId, walkBoolId;
     public static HandResourceInventory resourceInventory;
+
+    InputAction movingAction;
 
     void Start()
     {
         resourceInventory = this;
+
+        interactTriggerId = Animator.StringToHash(handInteractionTriggerName);
+        resetTriggerId = Animator.StringToHash(handResetTriggerName);
+        walkBoolId = Animator.StringToHash(handWalkBoolName);
+        movingAction = InputSystem.actions.FindAction("Move");
     }
 
     public bool TryToIncrementMeatCount()
@@ -17,8 +30,30 @@ public class HandResourceInventory : MonoBehaviour
         if(meatCount < maxMeatCount)
         {
             meatCount++;
+            animator.SetTrigger(interactTriggerId);
             return true;
         }
         return false;
+    }
+
+    /// <summary>
+    /// Clear player resource inventory. 
+    /// Returns count of resources that player caried
+    /// </summary>
+    public int ResourceTransfer()
+    {
+        int result = meatCount;
+        meatCount = 0;
+        animator.SetTrigger(resetTriggerId);
+        return result;
+    }
+
+    void Update()
+    {
+        //test call of crearing resources
+        if(Keyboard.current.tKey.wasPressedThisFrame) ResourceTransfer();
+        //operate walk animation
+        if(Time.frameCount % 4 == 0)
+        animator.SetBool(walkBoolId, movingAction.IsInProgress());  
     }
 }
