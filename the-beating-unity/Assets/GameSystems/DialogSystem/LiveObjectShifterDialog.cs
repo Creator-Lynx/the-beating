cc22bcccff82a6eaad5aa8324df604dc8783b528
CreatorLynx <1_linx_1@mail.ru>
2026-02-20 14:55:38 +0300

    start working on script control dymanic pos camera

---
+++ the-beating-unity/Assets/GameSystems/DialogSystem/LiveObjectShifterDialog.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LiveObjectShifterDialog : MonoBehaviour
+{
+  
+    [SerializeField] Vector2 shiftVector;
+    [SerializeField] float smoothTime = 0.1f;
+    [SerializeField] Vector2 defaultShift;
+
+
+    void Start()
+    {
+        defaultShift = transform.localPosition;
+    }
+    Vector3 currentShiftVelosity;
+    void Update()
+    {
+        float x = Mouse.current.position.ReadValue().x / Screen.width * 2 - 1;
+        float y = Mouse.current.position.ReadValue().y / Screen.height * 2 - 1;
+
+        Vector3 targetPos = defaultShift;
+        //targetPos += x * shiftVector.x * transform.right;
+        //targetPos += y * shiftVector.y * transform.up;
+        //targetPos.x = defaultShift.x + ;
+        //targetPos.y = defaultShift.y + ;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref currentShiftVelosity, smoothTime);
+    }
+}
