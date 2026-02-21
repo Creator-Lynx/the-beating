fa5229d292eb35ece1d0dbeed660483e4d023c31
CreatorLynx <1_linx_1@mail.ru>
2026-02-21 08:02:22 +0300

    mouse interactive dialog camera move fixed problem with vectors

--- the-beating-unity/Assets/GameSystems/DialogSystem/LiveObjectShifterDialog.cs
+++ the-beating-unity/Assets/GameSystems/DialogSystem/LiveObjectShifterDialog.cs
@@ -6,12 +6,12 @@ public class LiveObjectShifterDialog : MonoBehaviour
   
     [SerializeField] Vector2 shiftVector;
     [SerializeField] float smoothTime = 0.1f;
-    [SerializeField] Vector2 defaultShift;
+    [SerializeField] Vector3 defaultShift;
 
 
-    void Start()
+    void Awake()
     {
-        defaultShift = transform.localPosition;
+        defaultShift = transform.position;
     }
     Vector3 currentShiftVelosity;
     void Update()
@@ -20,8 +20,8 @@ public class LiveObjectShifterDialog : MonoBehaviour
         float y = Mouse.current.position.ReadValue().y / Screen.height * 2 - 1;
 
         Vector3 targetPos = defaultShift;
-        //targetPos += x * shiftVector.x * transform.right;
-        //targetPos += y * shiftVector.y * transform.up;
+        targetPos += x * shiftVector.x * transform.right;
+        targetPos += y * shiftVector.y * transform.up;
         //targetPos.x = defaultShift.x + ;
         //targetPos.y = defaultShift.y + ;
 
